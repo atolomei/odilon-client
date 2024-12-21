ab7bed6baa3f032681a5d668153081fde64533bd
Alejandro Tolomei <atolomei@novamens.com>
2024-12-21 13:57:44 -0300

    basetest

--- src/test/io/odilon/test/base/BaseTest.java
+++ src/test/io/odilon/test/base/BaseTest.java
@@ -74,7 +74,7 @@ public abstract class BaseTest {
 	private OdilonClient client;
 	private Bucket testBucket;
 
-	private int max = 10;
+	private int max = 20;
 	
 	private long max_length =120 * 100 * 10000; // 120 MB
 	
